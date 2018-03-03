3bf3d0fc6cf25281aad27bb030d118d6c9826f35
Jochen Klar <jochenklar@gmail.com>
2018-03-03 19:29:12 +0100

    Add marker, refactor code

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var map, data, markers = [];
+
+var airtable_url = 'https://api.airtable.com/v0/appM01mYlIJUkU1Od/',
+    airtable_key = 'keyl5v0iA9uirvIAH';
+
 var opt = {
     map: {
         url: 'https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
@@ -9,12 +13,31 @@ var opt = {
         }
     },
     location: {
-        center: [52.5036411,13.4265875], // Germany
+        center: [52.5036411,13.4265875],
         zoom: 15
+    },
+    icon: {
+        shadowUrl: 'img/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
     }
 };
 
+var icons = {
+    'Refill': new L.Icon($.extend({iconUrl: 'img/marker-icon-blue.png'}, opt.icon)),
+    'Testimonial': new L.Icon($.extend({iconUrl: 'img/marker-icon-green.png'}, opt.icon)),
+    'Test': new L.Icon($.extend({iconUrl: 'img/marker-icon-violet.png'}, opt.icon))
+};
+
 $(document).ready(function() {
+    var templates = {
+        'Refill': Handlebars.compile(document.getElementById('refill-template').innerHTML),
+        'Testimonial': Handlebars.compile(document.getElementById('testimonial-template').innerHTML),
+        'Test': Handlebars.compile(document.getElementById('test-template').innerHTML)
+    }
+
     map = L.map('map', {
         zoomControl: true
     });
@@ -22,66 +45,74 @@ $(document).ready(function() {
     L.tileLayer(opt.map.url, opt.map.options).addTo(map);
     map.setView(opt.location.center, opt.location.zoom);
 
-    var templates = {
-        'refill': Handlebars.compile(document.getElementById('refill-template').innerHTML),
-        'testimonial': Handlebars.compile(document.getElementById('testimonial-template').innerHTML),
-        'test': Handlebars.compile(document.getElementById('test-template').innerHTML)
-    };
-
     $.ajax({
         type: 'GET',
-        url: 'https://api.airtable.com/v0/appM01mYlIJUkU1Od/Refill',
-        headers: {
-            'Authorization': 'Bearer keyl5v0iA9uirvIAH'
-        },
-        success: function(response) {
-            var geojson = {
-                type: 'FeatureCollection',
-                features: []
-            };
-
-            $.each(response.records, function(index, record) {
-                var feature = {
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Point',
-                        coordinates: [
-                            record.fields['Längengrad'],
-                            record.fields['Breitengrad']
-                        ]
-                    },
-                    properties: {
-                        type: 'refill',
-                        name: record.fields['name']
-                    }
-                }
-
-                geojson.features.push(feature)
-            });
-
+        url: 'border.json',
+        success: function(geojson) {
             L.geoJSON(geojson, {
-                style: function(feature) {
-                    if (feature.properties.type == 'border') {
-                        return {
-                            'color': "#aa0000",
-                            'weight': 10,
-                            'opacity': .5,
-                            'fill': null
-                        };
-                    } else {
-                        return {};
-                    }
-                },
-                pointToLayer: function (feature, latlng) {
-                    var popup = templates[feature.properties.type](feature.properties);
-
-                    var options = {};
-
-                    marker = L.marker(latlng, options);
-                    marker.bindPopup(popup);
-                    return marker
+                style: {
+                    'color': "#aa0000",
+                    'weight': 10,
+                    'opacity': .5,
+                    'fill': null
                 }
             }).addTo(map);
         }
     });
+
+    $.each(['Refill', 'Testimonial', 'Test'], function(index, value) {
+        $.ajax({
+            type: 'GET',
+            url: airtable_url + value,
+            headers: {
+                'Authorization': 'Bearer ' + airtable_key
+            },
+            success: function(response) {
+                L.geoJSON(to_geojson(response), {
+                    pointToLayer: function(feature, latlng) {
+                        var popup = templates[value](feature.properties);
+
+                        var options = {};
+
+                        marker = L.marker(latlng, {icon: icons[value]});
+                        marker.bindPopup(popup);
+                        return marker
+                    }
+                }).addTo(map);
+            }
+        });
+    });
 });
+
+function to_geojson(response) {
+    var geojson = {
+        type: 'FeatureCollection',
+        features: []
+    };
+
+    $.each(response.records, function(index, record) {
+        var lon = record.fields['Länge'],
+            lat = record.fields['Breite'];
+
+        if (typeof(lon) !== 'undefined' || typeof(lat) !== 'undefined') {
+            var feature = {
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [lon, lat]
+                },
+                properties: {
+                    type: 'refill',
+                }
+            }
+
+            $.each(record.fields, function(key, value) {
+                feature.properties[key] = value;
+            });
+
+            geojson.features.push(feature)
+        }
+    });
+
+    return geojson;
+}
